54b921086f3b1aebd22bc487f90378439fb2770e
raissaalmaira <152668075+raissaalmaira@users.noreply.github.com>
2025-05-25 14:01:13 +0700

    Add files via upload
    video 3 : latihan3.html & script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+// let mahasiswa = {
+//     nama : "Raissa Almaira",
+//     nim : "2217020077",
+//     email : "raissarayy01@gmail.com"
+
+// }
+
+// console.log(JSON.stringify(mahasiswa));
+
+
+// let xhr = new XMLHttpRequest();
+// xhr.onreadystatechange = function () {
+//     if(xhr.readyState == 4 && xhr.status == 200) {
+//         let mahasiswa = JSON.parse(this.responseText); 
+//         console.log(mahasiswa);
+//     }
+// }
+// xhr.open('GET','coba.json', true);
+// xhr.send();
+
+$.getJSON('coba.json', function(data){
+    console.log(data);
+
+});
+
+
